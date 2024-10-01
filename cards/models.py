e9191791f5baa09ed09eda2b78fe95ace3f62d23
ArsenAbdiye <arsenabdiyev@gmail.com>
2024-10-01 11:08:32 +0300

    arsens

--- cards/models.py
+++ cards/models.py
@@ -42,7 +42,7 @@ class ItemImages(models.Model):
 
     class Meta:
         verbose_name = 'Изображение товара'
-        verbose_name_plural = 'Изображения товаsров'
+        verbose_name_plural = 'Изображенsия товаsров'
         ordering = ['order']
 
     def __str__(self):
