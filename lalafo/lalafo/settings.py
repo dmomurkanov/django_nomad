bf91f5fb963b53586be90e5e4aae53bb4c485712
omurkanovd22 <omurkanovd22@gmail.com>
2024-10-09 14:50:19 +0600

    templates

--- lalafo/lalafo/settings.py
+++ lalafo/lalafo/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = "lalafo.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR,],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
